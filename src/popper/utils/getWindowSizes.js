4a1917d1f9f5e4e993be049890e40bbbcfa17122
Federico Zivolo <fzivolo@quid.com>
2017-05-12 18:26:21 +0200

    [fix] Don't attempt to use scrollSize to detect window sizes
    Because we want the actual window size, not the document size.

--- src/popper/utils/getWindowSizes.js
+++ src/popper/utils/getWindowSizes.js
@@ -3,17 +3,13 @@ export default function getWindowSizes() {
   const html = window.document.documentElement;
   return {
     height: Math.max(
-      body.scrollHeight,
       body.offsetHeight,
       html.clientHeight,
-      html.scrollHeight,
       html.offsetHeight
     ),
     width: Math.max(
-      body.scrollWidth,
       body.offsetWidth,
       html.clientWidth,
-      html.scrollWidth,
       html.offsetWidth
     ),
   };
